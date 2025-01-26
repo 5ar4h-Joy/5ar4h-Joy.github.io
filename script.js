8f70a2d88fd2478e45f34446ea6aa78675d26631
5ar4h <68246013+5ar4h-Joy@users.noreply.github.com>
2025-01-26 15:16:00 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//JS file.
+function lightmode(){
+    document.getElementById("theme").className="lightmode";
+}
+function darkmode(){
+    document.getElementById("theme").className="darkmode";
+}
+function grayscale(){
+    document.getElementById("theme").className="grayscale";
+}
